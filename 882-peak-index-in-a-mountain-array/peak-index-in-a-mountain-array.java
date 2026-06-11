29630362305db562546fdc1e3eabc2fc4d79c2f8
devendra9955 <136847827+devendra9955@users.noreply.github.com>
2026-06-11 19:24:03 +0530

    Time: 0 ms (100.00%) | Memory: 80.5 MB (20.23%) - LeetSync

--- 882-peak-index-in-a-mountain-array/peak-index-in-a-mountain-array.java
+++ 882-peak-index-in-a-mountain-array/peak-index-in-a-mountain-array.java
@@ -3,16 +3,27 @@ class Solution {
         int lo = 0;
          int n = arr.length;
          int hi = n-1;
-         while(lo <= hi){
-            int mid = (lo + hi)/2;
-            if(mid-1 >=0 && mid+1 < n && arr[mid] > arr[mid+1] && arr[mid] < arr[mid-1]) hi = mid-1;
-            else if(mid-1 >=0 && mid+1 < n && arr[mid] < arr[mid+1] && arr[mid] > arr[mid-1]) lo = mid+1;
-            else if(mid-1 >=0 && mid+1 < n && arr[mid] > arr[mid+1] && arr[mid] > arr[mid-1]) return mid;
-             else {
-                if (mid == 0) lo = mid + 1;
-                else if (mid == n - 1) hi = mid - 1;
+        //  while(lo <= hi){
+        //     int mid = (lo + hi)/2;
+        //     if(mid-1 >=0 && mid+1 < n && arr[mid] > arr[mid+1] && arr[mid] < arr[mid-1]) hi = mid-1;
+        //     else if(mid-1 >=0 && mid+1 < n && arr[mid] < arr[mid+1] && arr[mid] > arr[mid-1]) lo = mid+1;
+        //     else if(mid-1 >=0 && mid+1 < n && arr[mid] > arr[mid+1] && arr[mid] > arr[mid-1]) return mid;
+        //      else {
+        //         if (mid == 0) lo = mid + 1;
+        //         else if (mid == n - 1) hi = mid - 1;
+        //     }
+        // }
+        // return -1;
+      
+        while (lo < hi) {
+            int mid = (lo + hi) / 2;
+
+            if (arr[mid] < arr[mid + 1]) {
+                lo = mid + 1;
+            } else {
+                hi = mid;
             }
         }
-        return -1;
+        return lo;
     }
 }
