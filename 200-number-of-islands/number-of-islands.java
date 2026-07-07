928b32b4a8202cc7f006df7d4c0af9f7e0240c3c
devendra9955 <136847827+devendra9955@users.noreply.github.com>
2026-07-07 18:31:48 +0530

    Time: 5 ms (29.22%) | Memory: 51.1 MB (95.26%) - LeetSync

--- 200-number-of-islands/number-of-islands.java
+++ 200-number-of-islands/number-of-islands.java
@@ -1,25 +1,54 @@
+class Pair{
+    int row;
+    int col;
+    Pair(int row, int col){
+        this.row = row;
+        this.col = col;
+    }
+}
 class Solution {
-    public void dfs(int i, int j , char[][] grid,boolean vis[][]){
+    public void bfs(int i, int j,char grid[][],boolean[][] vis){
         int m = grid.length;
         int n = grid[0].length;
         vis[i][j] = true;
-        if(i-1 >=0 && grid[i-1][j] == '1' && !vis[i-1][j])dfs(i-1, j, grid, vis);
-        if(i+1 <m && grid[i+1][j] == '1' && !vis[i+1][j])dfs(i+1, j, grid, vis);
-        if(j-1 >=0 && grid[i][j-1] == '1' && !vis[i][j-1])dfs(i, j-1, grid, vis);
-        if(j+1 <n && grid[i][j+1] == '1' && !vis[i][j+1])dfs(i, j+1, grid, vis);
+        Queue<Pair> q = new LinkedList<>();
+        q.add(new Pair(i,j));
+        while(q.size() > 0){
+            Pair front = q.remove();
+            int row = front.row;
+            int col = front.col;
+            if(row+1 < m && !vis[row+1][col] && grid[row+1][col]=='1') {
+                q.add(new Pair(row+1,col));
+                vis[row+1][col] = true;
+            }
+            if(row-1 >=0 && !vis[row-1][col] && grid[row-1][col] == '1') {
+                q.add(new Pair(row-1,col));
+                vis[row-1][col] = true;
+            }
+            if(col+1 <n && !vis[row][col+1] && grid[row][col+1] == '1') {
+                q.add(new Pair(row,col+1));
+                vis[row][col+1] = true;
+            }
+            if(col-1 >=0 && !vis[row][col-1] && grid[row][col-1]=='1'){
+                q.add(new Pair(row,col-1));
+                vis[row][col-1] = true;
+            }
+
+        }
 
     }
     public int numIslands(char[][] grid) {
         int m = grid.length;
         int n = grid[0].length;
-        boolean vis[][] = new boolean[m][n];
-        int count = 0;
+        int count=0;
+        boolean[][] vis = new boolean[m][n];
         for(int i=0; i<m; i++){
             for(int j=0; j<n; j++){
-                if(grid[i][j] == '1' && !vis[i][j]){
-                    dfs(i,j,grid,vis);
-                    count++;
+                if(!vis[i][j] && grid[i][j] =='1'){
+                     bfs(i,j,grid,vis);
+                     count++;
                 }
+               
             }
         }
         return count;
