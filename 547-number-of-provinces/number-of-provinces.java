1bee60834c7fe2318827974b2064c6b7b643cab0
devendra9955 <136847827+devendra9955@users.noreply.github.com>
2026-07-07 16:24:15 +0530

    Time: 1 ms (87.18%) | Memory: 47.2 MB (58.44%) - LeetSync

--- 547-number-of-provinces/number-of-provinces.java
+++ 547-number-of-provinces/number-of-provinces.java
@@ -1,27 +1,26 @@
 class Solution {
-    public void bfs(int i, boolean[] visited, int isConnected[][]){
-        Queue<Integer> q = new LinkedList<>();
+    public void bfs(int i,int isConnected[][],boolean[] vis){
         int n = isConnected.length;
+        Queue<Integer> q = new LinkedList<>();
+        vis[i] = true;
         q.add(i);
-        visited[i]=true;
-
-        while(q.size() >0){
+        while(q.size() > 0){
             int front = q.remove();
             for(int j=0; j<n; j++){
-                if(isConnected[front][j]==1 && visited[j]==false){
+                if(j != front && !vis[j] && isConnected[front][j]==1){
                     q.add(j);
-                    visited[j] = true;
+                    vis[j] = true;
                 }
             }
         }
     }
     public int findCircleNum(int[][] isConnected) {
-        int count = 0;
         int n = isConnected.length;
-        boolean[] visited = new boolean[n];
+        boolean[] vis = new boolean[n];
+        int count = 0;
         for(int i=0; i<n; i++){
-            if(!visited[i]){
-                bfs(i,visited,isConnected);
+            if(!vis[i]){
+                bfs(i,isConnected,vis);
                 count++;
             }
         }
