0e7f9d35893e96b834b1098e1f774919b5404ba0
devendra9955 <136847827+devendra9955@users.noreply.github.com>
2026-07-24 17:04:02 +0530

    Time: 0 ms (100.00%) | Memory: 46.5 MB (52.32%) - LeetSync

---
+++ 138-copy-list-with-random-pointer/copy-list-with-random-pointer.java
@@ -0,0 +1,48 @@
+/*
+// Definition for a Node.
+class Node {
+    int val;
+    Node next;
+    Node random;
+
+    public Node(int val) {
+        this.val = val;
+        this.next = null;
+        this.random = null;
+    }
+}
+*/
+
+class Solution {
+    public Node deepCopy(Node head1){
+        Node temp1 = head1;
+        Node head2 = new Node(-1);
+        Node temp2 = head2;
+        while(temp1 != null){
+            Node t = new Node(temp1.val);
+            temp2.next = t;
+            temp2 = temp2.next;
+            temp1=temp1.next;
+        }
+        return head2.next;
+    }
+    public Node copyRandomList(Node a) {
+        HashMap<Node,Node> map = new HashMap<>();
+        Node b = deepCopy(a);
+        Node tempA = a;
+        Node tempB=b;
+        map.put(null,null);
+        while(tempA != null){
+            map.put(tempA,tempB);
+            tempA = tempA.next;
+            tempB = tempB.next;
+        }
+        tempA = a;
+        while(tempA != null){
+            tempB = map.get(tempA);
+            tempB.random = map.get(tempA.random);
+            tempA = tempA.next;
+        }
+        return b;
+    }
+}
