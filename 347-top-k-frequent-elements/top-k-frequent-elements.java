3a5a0954b0cb773fb400ebceef4911937e8127c0
devendra9955 <136847827+devendra9955@users.noreply.github.com>
2026-07-25 14:43:53 +0530

    Time: 14 ms (70.82%) | Memory: 47.9 MB (19.57%) - LeetSync

---
+++ 347-top-k-frequent-elements/top-k-frequent-elements.java
@@ -0,0 +1,35 @@
+class Pair implements Comparable<Pair>{
+    int ele;
+    int freq;
+    Pair(int ele, int freq){
+        this.ele = ele;
+        this.freq = freq;
+    }
+    public int compareTo(Pair p){
+        if(this.freq == p.freq) return this.ele - p.ele;
+        return this.freq - p.freq;
+    }
+}
+
+class Solution {
+    public int[] topKFrequent(int[] nums, int k) {
+        HashMap<Integer,Integer> map = new HashMap<>();
+        int n = nums.length;
+        int idx = 0;
+        int[] arr = new int[k];
+        for(int ele : nums){
+            map.put(ele,map.getOrDefault(ele,0)+1);
+        }
+        PriorityQueue<Pair> pq = new PriorityQueue<>();
+        for(int ele : map.keySet()){
+            int freq = map.get(ele);
+            pq.add(new Pair(ele,freq));
+            if(pq.size() > k) pq.remove();
+        }
+        while(pq.size() > 0){
+            Pair top = pq.remove();
+            arr[idx++] = top.ele;
+        }
+        return arr;
+    }
+}
