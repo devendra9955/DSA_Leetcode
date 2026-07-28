dff747086bcf2c572714502dd9ba537d3e080cc1
devendra9955 <136847827+devendra9955@users.noreply.github.com>
2026-07-28 16:54:12 +0530

    Time: 124 ms (87.54%) | Memory: 112.2 MB (8.30%) - LeetSync

--- 295-find-median-from-data-stream/find-median-from-data-stream.java
+++ 295-find-median-from-data-stream/find-median-from-data-stream.java
@@ -7,23 +7,19 @@ class MedianFinder {
     }
     
     public void addNum(int num) {
-        if(maxHeap.size() == 0 || num < maxHeap.peek()) maxHeap.add(num);
-        else minHeap.add(num);
-        if(maxHeap.size() == minHeap.size()+2) minHeap.add(maxHeap.remove());
-        if(minHeap.size() == maxHeap.size()+2) maxHeap.add(minHeap.remove());
+        if(maxHeap.size()==0) maxHeap.add(num);
+        else{
+            if(num<maxHeap.peek()) maxHeap.add(num);
+            else minHeap.add(num);
+        }
+        if(maxHeap.size()==minHeap.size()+2) minHeap.add(maxHeap.remove());
+        if(minHeap.size()==maxHeap.size()+2) maxHeap.add(minHeap.remove());
     }
     
     public double findMedian() {
-        if(maxHeap.size() == minHeap.size() + 1)
-            return maxHeap.peek();
-
-        else if(minHeap.size() == maxHeap.size() + 1)
-            return minHeap.peek();
-
-        else
-            return (maxHeap.peek() + minHeap.peek()) / 2.0;
-    
-
+        if(maxHeap.size()==minHeap.size()+1) return maxHeap.peek();
+        else if(minHeap.size()==maxHeap.size()+1) return minHeap.peek();
+        else return ((maxHeap.peek()+minHeap.peek())/2.0);
     }
 }
 
