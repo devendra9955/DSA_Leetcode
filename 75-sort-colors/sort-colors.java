10cd705272409c72b61fde998d823edf66a97d78
devendra9955 <136847827+devendra9955@users.noreply.github.com>
2026-08-08 14:51:52 +0530

    Time: 0 ms (100.00%) | Memory: 43.5 MB (43.64%) - LeetSync

--- 75-sort-colors/sort-colors.java
+++ 75-sort-colors/sort-colors.java
@@ -1,51 +1,29 @@
 class Solution {
     public void sortColors(int[] nums) {
-        // int lo = 0;
-        // int mid = 0;
-        // int hi = nums.length-1;
-        // while(mid <= hi){
-        //     if(nums[mid]==0){
-        //         int temp = nums[lo];
-        //         nums[lo] = nums[mid];
-        //         nums[mid] = temp;
-        //         lo++;
-        //         mid++;
-        //     }
-        //     else if(nums[mid]==2){
-        //         int temp = nums[hi];
-        //         nums[hi] = nums[mid];
-        //         nums[mid] = temp;
-        //         hi--;
-        //     }
-        //     else mid++;
-        // }
-        int z = 0;
-        int o = 0;
-        int t = 0;
         int n = nums.length;
-        for(int i=0; i<n; i++){
-            if(nums[i]==0){
-                z++;
-            }
-            else if(nums[i]==1){
-                o++;
-            }
-            else if(nums[i]==2){
-                t++;
-            }
+        int countzero = 0;
+        int countones = 0;
+        int counttwo = 0;
+        int i = 0;
+        for(int j=0; j<n; j++){
+            if(nums[j]==0)countzero++;
+            if(nums[j]==1) countones++;
+            if(nums[j]==2) counttwo++; 
         }
-        int i=0;
-        while(z-- > 0){
-            nums[i]=0;
+        while(countzero>0){
+            nums[i] = 0;
             i++;
+            countzero--;
         }
-        while(o-- > 0){
-            nums[i]=1;
+        while(countones>0){
+            nums[i] = 1;
             i++;
+            countones--;
         }
-        while(t-- >0){
-            nums[i]=2;
+        while(counttwo>0){
+            nums[i] = 2;
             i++;
+            counttwo--;
         }
     }
 }
